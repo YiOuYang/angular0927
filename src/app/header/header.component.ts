bee561ad7d030773964e86ede02d1c1b62deb1b5
Will <doggy.huang@gmail.com>
2018-09-27 13:45:53 +0800

    示範屬性繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,7 @@ import { environment } from '../../environments/environment';
 export class HeaderComponent implements OnInit {
 
   sitename = 'Hello World';
+  sitelink = 'http://blog.miniasp.com/';
 
   constructor() { }
 
